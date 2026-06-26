11bf4f525e9ffa812dc1f5534f45659c30c1b373
PRIYANSHU <priyanshuask528@gmail.com>
2026-06-26 08:35:05 +0530

    Intial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+
+
+print("Enter 1 for creating data .")
+print("Enter 2 for reading data . ")
+print("Enter 3 for updating data .")
+print("Enter 4 for deleting data .")
+
+op=int(input("Enter your choise:- "))
+
+if(op==1):
+    creat()
+elif(op==2):
+
+
+elif(op==3):
+
+
+elif(op==4):
+
+else :
+    print("Entered choise is not valid.Re-enter.")
