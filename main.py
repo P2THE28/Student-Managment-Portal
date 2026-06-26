94abad2627c00a1538b8cf5401344ec0e576a13b
PRIYANSHU <priyanshuask528@gmail.com>
2026-06-26 08:51:05 +0530

    All choises option is add and also data file is  added

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import json
+from pathlib import Path
 
 
-print("Enter 1 for creating data .")
-print("Enter 2 for reading data . ")
-print("Enter 3 for updating data .")
-print("Enter 4 for deleting data .")
+print("Enter 1 for Adding New student Details.")
+print("Enter 2 for Seeing Details of existing student. ")
+print("Enter 3 for updating Details .")
+print("Enter 4 for Deleting student data .")
 
 op=int(input("Enter your choise:- "))
 
 if(op==1):
-    creat()
+    Student.add()
 elif(op==2):
-
-
+    Student.details()
 elif(op==3):
-
-
+    Student.update()
 elif(op==4):
-
+    studen.delete()
 else :
-    print("Entered choise is not valid.Re-enter.")
+    print("Entered choise is not valid.")
